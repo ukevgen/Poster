abd3ad16aab64717351918114744907b84bae181
Eugene Surkov <eugene.surkov@agilie.com>
2017-07-05 18:36:17 +0300

    Fragment text in work

--- app/src/main/java/com/agilie/poster/view/fragments/text/TextIconFragment.kt
+++ app/src/main/java/com/agilie/poster/view/fragments/text/TextIconFragment.kt
@@ -1,6 +1,16 @@
 package com.agilie.poster.view.fragments.text
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.agilie.poster.R
 
 class TextIconFragment : Fragment() {
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		val view = inflater.inflate(R.layout.fragment_text, container, false)
+
+		return view
+	}
 }
