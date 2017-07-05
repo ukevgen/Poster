30a615996d1a0662654c8dbd1808d64aad9cdb6f
Eugene Surkov <eugene.surkov@agilie.com>
2017-07-05 16:04:30 +0300

    Fragment Icons and Shape Box is completed

--- app/src/main/java/com/agilie/poster/view/fragments/icons/IconsFragment.kt
+++ app/src/main/java/com/agilie/poster/view/fragments/icons/IconsFragment.kt
@@ -8,9 +8,18 @@ import android.view.ViewGroup
 import com.agilie.poster.R
 
 class IconsFragment : Fragment() {
+
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		val view = inflater.inflate(R.layout.fragment_icons, container, false)
 
 		return view
+	}
+
+	override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+		init()
+	}
+
+	private fun init() {
+
 	}
 }
