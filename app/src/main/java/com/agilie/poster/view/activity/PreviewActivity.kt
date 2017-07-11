26c6bccd8f7b9b8944c94a2abbe05cbeb39180b0
Eugene Surkov <eugene.surkov@agilie.com>
2017-07-11 10:44:42 +0300

    Update camera activity

---
+++ app/src/main/java/com/agilie/poster/view/activity/PreviewActivity.kt
@@ -0,0 +1,12 @@
+package com.agilie.poster.view.activity
+
+import android.os.Bundle
+import com.agilie.poster.R
+
+class PreviewActivity : BaseActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_preview)
+	}
+}
